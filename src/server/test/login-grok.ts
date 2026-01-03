fcbd321e77dfef737f463fb594626ed3c111eb83
ozan-fn <me@ozan.my.id>
2026-01-03 22:13:40 +0700

    feat: update login scripts to wait for profile image and improve error handling

--- src/server/test/login-grok.ts
+++ src/server/test/login-grok.ts
@@ -14,7 +14,7 @@ import CookieModel from "../models/cookieModel.js";
         const page = await browser.newPage();
         await page.goto("https://accounts.x.ai/sign-in?redirect=grok-com");
 
-        await page.waitForSelector("textarea", { timeout: 0, visible: true });
+        await page.waitForSelector("img[alt='pfp']", { timeout: 0, visible: true });
 
         const cookies = await page.cookies();
         await CookieModel.findOneAndUpdate({ type: "grok" }, { type: "grok", cookies: JSON.stringify(cookies) }, { upsert: true });
--- src/server/test/test-grok.ts
+++ src/server/test/test-grok.ts
@@ -1,19 +1,30 @@
-import { getBrowser } from "../shared/browser";
-import fs from "fs";
+import { config } from "dotenv";
+config();
+
+import { getBrowser } from "../shared/browser.js";
+import mongoose from "mongoose";
+import CookieModel from "../models/cookieModel.js";
 
 (async () => {
-    const browser = await getBrowser();
-    const page = await browser.newPage();
-    await await page.goto("https://grok.com");
+    try {
+        await mongoose.connect(process.env.DATABASE_URL!);
 
-    if (await page.$('a[href="/sign-in"]')) {
-        await page.click('a[href="/sign-in"]');
+        const browser = await getBrowser();
+        const page = await browser.newPage();
+        await page.goto("https://grok.com");
+
+        if (await page.$('a[href="/sign-in"]')) {
+            await page.click('a[href="/sign-in"]');
+        }
+
+        await page.waitForSelector("img[alt='pfp']", { timeout: 0 });
+
+        const cookies = await page.cookies();
+        await CookieModel.findOneAndUpdate({ type: "grok" }, { type: "grok", cookies: JSON.stringify(cookies) }, { upsert: true });
+
+        await browser.close();
+        await mongoose.disconnect();
+    } catch (error) {
+        console.error("Error:", error);
     }
-
-    await page.waitForSelector("textarea", { timeout: 0, visible: true });
-
-    const cookies = await page.cookies();
-    fs.writeFileSync("./cookies-grok.json", JSON.stringify(cookies, null, 2));
-
-    await browser.close();
 })();
