ccf8e414f77781eac1e026701e0fca9fd2a6b8ff
ozan-fn <me@ozan.my.id>
2026-01-31 17:08:42 +0700

    .

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
 import path from "path";
+import compression from "compression";
 import apiRoutes from "./routes/api";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(compression({ level: 6 }));
 app.use(express.json());
 
 // API Routes
